569654cccf4abda11be9e6bd7d504d000edb0123
Thiago Henrique F. Margonar <thiago.margonar@hotmail.com>
2022-10-25 09:25:25 -0300

    iniciando testes unitários

--- src/main/java/com/curso/webflux/webflux/aluno/service/TesteProducerBean.java
+++ src/main/java/com/curso/webflux/webflux/aluno/service/TesteProducerBean.java
@@ -2,6 +2,10 @@ package com.curso.webflux.webflux.aluno.service;
 
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
 
 import java.util.function.Function;
@@ -10,19 +14,45 @@ import java.util.function.Supplier;
 @Component
 public class TesteProducerBean {
 
+//    @Bean
+//    public Supplier<String> supplier() {
+//        return () -> {
+//            System.out.println("Sending Hello World suplier");
+//            return "Hello World - suplier";
+//        };
+//    }
+//
+//    @Bean
+//    public Function<String, String> uppercase() {
+//        return value -> {
+//            System.out.println("received **> "+value);
+//            return value.toUpperCase();
+//        };
+//    }
+
     @Bean
-    public Supplier<String> supplier() {
+    public Supplier<Message<String>> supplier() {
         return () -> {
             System.out.println("Sending Hello World suplier");
-            return "Hello World - suplier";
+
+            return MessageBuilder.withPayload("Hello World suplier")
+                    .setHeader("bar", "baz")
+                    .build();
+
         };
     }
 
     @Bean
-    public Function<String, String> uppercase() {
-        return value -> {
-            System.out.println("received **> "+value);
-            return value.toUpperCase();
+    public Function<Message<String>, Message<String>> uppercase() {
+        return message -> {
+            Acknowledgment acknowledgment = message.getHeaders().get(KafkaHeaders.ACKNOWLEDGMENT, Acknowledgment.class);
+            if (acknowledgment != null) {
+                System.out.println("Acknowledgment provided");
+                acknowledgment.acknowledge();
+            }
+
+            System.out.println("received **> " + message);
+            return message;
         };
     }
 }
